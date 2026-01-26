8ae224b37145db727d7d727517b742934d6d7645
xajimmar560 <xajimmar560@ieshnosmachado.org>
2026-01-26 23:43:11 +0100

    workflow

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
+from . import servicio
+from . import repuesto
 from . import vehiculo
 from . import orden_reparacion
 from . import persona
 from . import mecanico
-from . import servicio
-from . import repuesto
 from . import cliente
--- models/servicio.py
+++ models/servicio.py
@@ -1,14 +1,38 @@
-from odoo import models, fields, api
-
 class Servicio(models.Model):
     _name = 'taller.servicio'
-    _descripcion = 'Servicios relacionados con reparaciones'
+    _description = 'Servicios relacionados con reparaciones'
     _rec_name = 'tipo'
 
-    coste = fields.Float(string='Coste', required = True)
+    coste = fields.Float(string='Coste', required=True)
     tipo = fields.Selection([
         ('reponer', 'Reponer stock'),
         ('limpiado', 'Limpiado'),
-        ('cristales', 'Cambiar los cristales')],
-        string='Tipo de servicio', required = True)
-    fecha = fields.Date(string='Fecha del servicio', required = True)
+        ('cristales', 'Cambiar los cristales')
+    ], string='Tipo de servicio', required=True)
+    
+    fecha = fields.Date(string='Fecha del servicio', required=True)
+
+    # El campo state
+    state = fields.Selection([
+        ('borrador', 'Borrador'),
+        ('en_proceso', 'En Proceso'),
+        ('terminado', 'Terminado'),
+        ('cancelado', 'Cancelado'),
+    ], string='Estado', default='borrador', required=True)
+
+    # Las funciones (alineadas dentro de la clase)
+    def action_en_proceso(self):
+        for rec in self:
+            rec.state = 'en_proceso'
+
+    def action_terminar(self):
+        for rec in self:
+            rec.state = 'terminado'
+
+    def action_cancelar(self):
+        for rec in self:
+            rec.state = 'cancelado'
+            
+    def action_borrador(self):
+        for rec in self:
+            rec.state = 'borrador'
