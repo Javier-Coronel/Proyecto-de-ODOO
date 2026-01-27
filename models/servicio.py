cd8d7b483260fb19f6f033290b8b0c73e2d43609
JuanCarlosc4 <xjcasoje079@ieshnosmachado.org>
2026-01-27 10:03:51 +0100

    Solución errores

--- models/servicio.py
+++ models/servicio.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
 class Servicio(models.Model):
     _name = 'taller.servicio'
     _description = 'Servicios relacionados con reparaciones'
